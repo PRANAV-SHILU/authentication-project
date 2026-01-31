b2fe932ef4b6340726de07236bb1edbfda73c0f5
PRANAV SHILU <pranavshilu1811@gmail.com>
2026-01-31 21:02:44 +0530

    deletd useless line

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,12 +1,13 @@
-import { NavLink } from "react-router-dom"
-import "../assets/css/pagenotfound.css"
-export default function PageNotFound(){
-    return(
-        <section>
-            <h1>Page not found - 404</h1>
-            <h2>kindly redirect to home page</h2>
-            <h3><NavLink to='/'>Home</NavLink></h3>
-            <h4>-- Team Pranav</h4>
-        </section>
-    )
+import { NavLink } from "react-router-dom";
+import "../assets/css/pagenotfound.css";
+export default function PageNotFound() {
+  return (
+    <section>
+      <h1>Page not found - 404</h1>
+      <h2>kindly redirect to home page</h2>
+      <h3>
+        <NavLink to="/">Home</NavLink>
+      </h3>
+    </section>
+  );
 }
